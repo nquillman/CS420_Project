85239941d12a730c0f831694d065a60bf17b2429
akononov <akononov@mit.edu>
2016-11-25 14:57:27 -0600

    Create A_minus_LU.c

---
+++ A_minus_LU.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+/* This function computes the product of a lower triangular matrix and an upper triangular matrix, and then subtracts that product
+from a regular dense matrix. It will be used to update blocks of A after we've solved for a pair of L and U blocks whose product
+contributes to that block of A. The function will be executed on a single processor using OpenMP for parallelization.*/
+
+int main() {
+  
+  return 0;
+}
