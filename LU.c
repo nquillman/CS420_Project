1f96904b5277cff4ef79bded2d04a1c34cc7dda4
akononov <akononov@mit.edu>
2016-11-25 14:41:39 -0600

    Update LU.c

--- LU.c
+++ LU.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 
+/* This function computes the LU decomposition of a matrix block using Guassian elimination. It will be used
+to factor the diagonal blocks of the matrix and it will execute on a single processor using OpenMP for parallelization. */
+
+int main() {
+  
+  return 0;
+}
