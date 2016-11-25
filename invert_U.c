9bfb27d8425386af045ebea31b549894f2d56d9c
akononov <akononov@mit.edu>
2016-11-25 14:45:57 -0600

    Create invert_U.c

---
+++ invert_U.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+/* This function computes the inverse of an upper triangular matrix using back substitution. This will be used to invert
+the diagnal blocks of U, which is the first step in solving for the off-diagonal blocks of L. The function will be executed
+on a single processor using OpenMP for parallelization.*/
+
+int main() {
+  
+  return 0;
+}
