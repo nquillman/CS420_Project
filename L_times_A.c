a43648a289467381b49173b764bb3d97b402483a
akononov <akononov@mit.edu>
2016-11-25 14:50:46 -0600

    Update and rename matrix_product.c to L_times_A.c

---
+++ L_times_A.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+/* This function computes the product of a lower triangular matrix and a regular dense matrix. It will be used to multiply the
+inverse of a diagonal block of L (produced by invert_L.c) by an off-diagonal block of A and thereby solve for an off-diagonal
+block of U. The function will be executed on a single processor using OpenMP for parallelization.*/
+
+int main() {
+  
+  return 0;
+}
