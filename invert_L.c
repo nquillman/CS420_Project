9b89bf9ae5d7fd33a0fbd97380d62956c896f1f7
ww785612 <li.dane@hotmail.com>
2016-11-28 00:46:29 -0600

    Update invert_L.c

--- invert_L.c
+++ invert_L.c
@@ -9,3 +9,30 @@ int main() {
   
   return 0;
 }
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(float* matrix, float* inverse) {
+  //matrix[i][j]
+  float temp_sum=0;
+  for(int j=0; j<=max;j++)
+  {
+       inverse[j][j]=1/(matrix[j][j]);
+    
+      for(int i=j+1; i<=max; i++)
+      {
+        {
+          for(int k=j;k<i;k++)
+          {
+              temp_sum-=matrix[i][k]*inverse[k][j];
+          }
+          inverse[i][j]=temp_sum/matrix[i][i];
+          
+        }
+          temp_sum=0;
+      }
+  }
+  return 0;
+}
