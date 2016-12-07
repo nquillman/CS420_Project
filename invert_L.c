d1022bdf879f2150cbdc645ffa5360e390a42881
akononov <akononov@mit.edu>
2016-12-07 15:52:53 -0600

    Update invert_L.c

--- invert_L.c
+++ invert_L.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 /* This function computes the inverse of a lower triangular matrix using forward substitution. It will be used to invert
 the diagnal blocks of L, which is the first step in solving for the off-diagonal blocks of U. The function will be executed
 on a single processor using OpenMP for parallelization.*/
 
-int main(double* matrix, double* inverse) {
+void invert_L(double* matrix, double* inverse) {
   //matrix[i][j]
   double temp_sum=0;
   for(int j=0; j<=max;j++)
@@ -25,5 +26,4 @@ int main(double* matrix, double* inverse) {
           temp_sum=0;
       }
   }
-  return 0;
 }
