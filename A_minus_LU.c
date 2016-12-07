9a1ca55d77dbf722613c0df302dada55f63a69f8
akononov <akononov@mit.edu>
2016-12-07 08:14:06 -0600

    Update A_minus_LU.c

--- A_minus_LU.c
+++ A_minus_LU.c
@@ -5,7 +5,19 @@
 from a regular dense matrix. It will be used to update blocks of A after we've solved for a pair of L and U blocks whose product
 contributes to that block of A. The function will be executed on a single processor using OpenMP for parallelization.*/
 
-int main() {
-  
-  return 0;
+void AmLU(float* A, float* L, float* U, int M, int N, int K) {
+  // A is MxN
+  // L is MxK
+  // U is KxN
+
+  // iterate over entries of A
+  int i, j ,k;
+  for (i=0; i<M; i++) {
+    for (j=0; j<N; j++) {
+      // iterate along row of L/column of U
+      for (k=0; k<fmin(j+1,i+1); k++) {
+       A[i*N+j] -= L[i*K+k]*U[k*N+j];   // subtract L[i,k]*U[k,j]
+      }
+    }
+  }
 }
