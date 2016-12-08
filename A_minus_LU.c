5effa1df50dfcb92f1751017a7489a6a4f41bdbb
akononov <akononov@mit.edu>
2016-12-08 14:03:17 -0600

    Update A_minus_LU.c

--- A_minus_LU.c
+++ A_minus_LU.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-/* This function computes the product of a lower triangular matrix and an upper triangular matrix, and then subtracts that product
-from a regular dense matrix. It will be used to update blocks of A after we've solved for a pair of L and U blocks whose product
-contributes to that block of A. The function will be executed on a single processor using OpenMP for parallelization.*/
+/* This functions compute the product of two dense matrices (L and U blocks) and then subtract that product from
+a dense matrix (A block). It will be used to update blocks of A after we've solved for a pair of off-diagonal L and U
+blocks whose product contributes to that block of A. The function will be executed on a single processor using OpenMP
+for parallelization.*/
 
 void AmLU(float* A, float* L, float* U, int M, int N, int K) {
   // A is MxN
@@ -17,7 +18,7 @@ void AmLU(float* A, float* L, float* U, int M, int N, int K) {
   for (i=0; i<M; i++) {
     for (j=0; j<N; j++) {
       // iterate along row of L/column of U
-      for (k=0; k<fmin(fmin(i+1,K),j+1); k++) {
+      for (k=0; k<K; k++) {
        A[i*N+j] -= L[i*K+k]*U[k*N+j];   // subtract L[i,k]*U[k,j]
       }
     }
